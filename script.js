c80db4bfa390dbd6342ef4712a83cc68c893c6fc
ManfredNelvin <119521670+ManfredNelvin@users.noreply.github.com>
2023-06-25 07:44:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-function getFormvalue() {
-    //Write your code here
+function getFormValue() {
+  // Retrieve the form element
+  const form = document.getElementById('myForm');
 
+  // Retrieve the values of the first name and last name input fields
+  const firstName = form.elements['firstName'].value;
+  const lastName = form.elements['lastName'].value;
+
+  // Display the first and last name using alert
+  alert('First Name: ' + firstName + '\nLast Name: ' + lastName);
 }
